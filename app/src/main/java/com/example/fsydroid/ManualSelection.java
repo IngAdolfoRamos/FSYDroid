80817f3c3af8753e6647c3593a6eef3e6471aaa0
Adolfo Ramos <ingadolfo@icloud.com>
2022-07-11 17:49:12 -0500

    Creating QR and Manual selection to save records

---
+++ app/src/main/java/com/example/fsydroid/ManualSelection.java
@@ -0,0 +1,14 @@
+package com.example.fsydroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManualSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manual_selection);
+    }
+}
