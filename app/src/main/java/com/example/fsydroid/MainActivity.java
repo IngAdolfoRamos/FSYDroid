6fe48c73eadb5eedeac6b31e8b1077cd472e3971
Adolfo Ramos <ingadolfo@icloud.com>
2022-07-11 21:21:02 -0500

    Retrieving selected event value in QRActivity from MainActivity

--- app/src/main/java/com/example/fsydroid/MainActivity.java
+++ app/src/main/java/com/example/fsydroid/MainActivity.java
@@ -6,11 +6,13 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int STORAGE_PERMISSION_CODE = 101;
 
     Spinner eventSelectionS;
+    Button qrB, manualSelectionB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         eventSelectionS = findViewById(R.id.actionSpinner);
+        qrB = findViewById(R.id.qrB);
+        manualSelectionB = findViewById(R.id.manualB);
 
         checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
 
@@ -45,13 +50,29 @@ public class MainActivity extends AppCompatActivity {
         eventSelectionS.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-//                String item = items.get(i);
                 Long item = adapterView.getSelectedItemId() + 1;
                 Toast.makeText(MainActivity.this, item.toString(), Toast.LENGTH_SHORT).show();
+
+                qrB.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent goToQRPage = new Intent(MainActivity.this, QR.class);
+                        goToQRPage.putExtra("eventValue", item);
+                        startActivity(goToQRPage);
+                    }
+                });
+
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        manualSelectionB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
             }
         });
--- app/src/main/java/com/example/fsydroid/QR.java
+++ app/src/main/java/com/example/fsydroid/QR.java
@@ -2,7 +2,9 @@ package com.example.fsydroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class QR extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class QR extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
+
+        Long eventValue;
+
+        Intent getEventValue = getIntent();
+        eventValue = getEventValue.getLongExtra("eventValue",0);
+
+        Toast.makeText(QR.this, "Valor recuperado: " + eventValue, Toast.LENGTH_LONG).show();
+
     }
 }
