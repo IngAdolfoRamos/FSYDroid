fd1d9e282ecad207b531a98b8eb6a8e999636b91
Adolfo Ramos <ingadolfo@icloud.com>
2022-07-12 01:44:23 -0500

    Creating SQLiteHelper to create BD

---
+++ app/src/main/java/com/example/fsydroid/SQLiteHelper.java
@@ -0,0 +1,26 @@
+package com.example.fsydroid;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+    final String CREATE_USER_TABLE = "CREATE TABLE People (Id INTEGER, Reference TEXT)";
+
+    public SQLiteHelper(@Nullable Context context, @Nullable String name,
+                        @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_USER_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    }
+}
