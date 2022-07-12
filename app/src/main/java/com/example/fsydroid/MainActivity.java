7c35906824208faee33125d22b29143c9ddd0dfc
Adolfo Ramos <ingadolfo@icloud.com>
2022-07-11 21:06:31 -0500

    Gettin selected id from spinner

--- app/src/main/java/com/example/fsydroid/MainActivity.java
+++ app/src/main/java/com/example/fsydroid/MainActivity.java
@@ -8,20 +8,54 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int CAMERA_PERMISSION_CODE = 100;
     private static final int STORAGE_PERMISSION_CODE = 101;
 
+    Spinner eventSelectionS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        eventSelectionS = findViewById(R.id.actionSpinner);
+
         checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, STORAGE_PERMISSION_CODE);
 
+        ArrayList<String> items = new ArrayList<>();
+        items.add("Ingreso");
+        items.add("Desayuno");
+        items.add("Comida");
+        items.add("Cena");
+        items.add("Salida");
+        items.add("Conteo nocturno");
+
+        eventSelectionS.setAdapter(new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, items));
+        
+        eventSelectionS.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+//                String item = items.get(i);
+                Long item = adapterView.getSelectedItemId() + 1;
+                Toast.makeText(MainActivity.this, item.toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        
     }
 
     public void checkPermission(String permission, int requestCode)
