da1bde0d9603126b211f6117678eacc40a9fcd96
Adolfo Ramos <ingadolfo@icloud.com>
2022-07-12 23:14:21 -0500

    Getting selected items from recicler view

--- app/src/main/java/com/example/fsydroid/ManualSelection.java
+++ app/src/main/java/com/example/fsydroid/ManualSelection.java
@@ -1,6 +1,8 @@
 package com.example.fsydroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
@@ -9,6 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -17,15 +20,16 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ManualSelection extends AppCompatActivity {
+public class ManualSelection extends AppCompatActivity implements QuantityListener{
 
     Spinner groupSelectorS;
-    TextView nameTV;
-    CheckBox nameSelectedCB;
     ArrayList<String> groups;
     ArrayList<Person> ListPeople;
+    RecyclerView peopleListRV;
+    Button saveDataB;
 
     SQLiteHelper connection;
+    QuantityAdapter quantityAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +39,8 @@ public class ManualSelection extends AppCompatActivity {
         connection = new SQLiteHelper(this, "FSY", null, 1);
 
         groupSelectorS = findViewById(R.id.groupSelectorS);
-        nameTV = findViewById(R.id.nameTV);
-        nameSelectedCB = findViewById(R.id.nameSelectedCB);
+        peopleListRV  = findViewById(R.id.peopleListRV);
+        saveDataB = findViewById(R.id.saveDataB);
 
         Long eventValue;
         Intent getEventValue = getIntent();
@@ -49,8 +53,8 @@ public class ManualSelection extends AppCompatActivity {
         String reference = "7501593830225";
         String[] ref = {reference};
 
-        SQLiteDatabase readGroups = connection.getReadableDatabase();
-
+        /* Obtener las referencias o grupos para llenar el Picker pero se va a harcodear */
+/*        SQLiteDatabase readGroups = connection.getReadableDatabase();
         Cursor cursor = readGroups.rawQuery("SELECT Id,Reference FROM Person",null);
         cursor.moveToFirst();
         groupsArray.add("agregado manual");
@@ -66,7 +70,7 @@ public class ManualSelection extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 this, android.R.layout.simple_spinner_item, groupsArray);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-        groupSelectorS.setAdapter(adapter);
+        groupSelectorS.setAdapter(adapter);*/
 
         groupSelectorS.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -80,10 +84,35 @@ public class ManualSelection extends AppCompatActivity {
             }
         });
 
+        setRecyclerView();
+
     }
 
-    private void getGroupList(){
-        SQLiteDatabase db = connection.getReadableDatabase();
+    private ArrayList<String> getQauntityData(){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        arrayList.add("4");
+        arrayList.add("5");
+        arrayList.add("6");
+        arrayList.add("7");
+        return arrayList;
+    }
+    private void setRecyclerView() {
+        peopleListRV.setHasFixedSize(true);
+        peopleListRV.setLayoutManager(new LinearLayoutManager(this));
+        quantityAdapter = new QuantityAdapter(this, getQauntityData(), this);
+        peopleListRV.setAdapter(quantityAdapter);
+    }
 
+    @Override
+    public void onQuantityChange(ArrayList<String> arrayList) {
+        saveDataB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ManualSelection.this, "Seleccion de id: " + arrayList.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/fsydroid/QuantityListener.java
@@ -0,0 +1,7 @@
+package com.example.fsydroid;
+
+import java.util.ArrayList;
+
+public interface QuantityListener {
+    void onQuantityChange(ArrayList<String> arrayList);
+}
